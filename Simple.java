08c7dfd0d486c32025858ebbbe96a9110452c2f4
SamadHFFC <samadali.nabizada@homefirstindia.com>
2021-01-31 01:14:21 +0530

    Testing

---
+++ Simple.java
@@ -0,0 +1,12 @@
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}
+
+git remote add origin <git url>
+ git pull -r origin master
+git push -u origin master```
+
+dfgjkhdfjg
+dfgdfhkjghkj
